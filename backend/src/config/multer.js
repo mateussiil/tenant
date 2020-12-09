0575239fbb9466d999bf3ec9b260729ae4e41f4e
Mateus Oliveira <mateussiiloliveira@gmal.com>
2020-12-09 20:30:33 -0300

    mudando para salvar arquivo em local

--- backend/src/config/multer.js
+++ backend/src/config/multer.js
@@ -38,7 +38,7 @@ const storageTypes = {
 
 module.exports = {
   dest: path.resolve(__dirname, "..", "..", "tmp", "uploads"),
-  storage: storageTypes[local],
+  storage: storageTypes[storageTypes.local],
   limits: {
     fileSize: 2 * 1024 * 1024
   },
